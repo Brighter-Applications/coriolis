88f635247f1d70c49788bddc082801e99347db6a
Alex Williams <alex.williams@brighter-technology.com>
2024-11-06 20:06:56 +0000

    Add mandalay (#36)
    * Remove link to squatted domain
    
    `http://www.edshipyard.com/` is now owned by a Vietnamese betting company (Mu88). Remove the link to avoid giving them traffic.
    
    I considered replacing the link with an archived version, but, for me, 
    https://web.archive.org/web/20231030223647/http://www.edshipyard.com/ just shows a mostly-white page with some light grey text at the top, so it didn't seem useful.
    
    * Initial Mandalay commit
    
    * Changing date on Announcement of Mandalay
    
    * Fixing announcement layout and presentation and expiry control
    
    ---------
    
    Co-authored-by: Chris <2653277+chennin@users.noreply.github.com>

--- src/app/Coriolis.jsx
+++ src/app/Coriolis.jsx
@@ -68,7 +68,9 @@ export default class Coriolis extends React.Component {
     this.state = {
       noTouch: !('ontouchstart' in window || navigator.msMaxTouchPoints || navigator.maxTouchPoints),
       page: null,
-      announcements: [{expiry: "30-11-2024", text: "31/10/2024: Mandalay added to the shipyard"}],
+      // Announcements must have an expiry date in format "YYYY-MM-DDTHH:MM:SSZ"
+      announcements: [{expiry: "2024-11-30T00:00:00Z", text: "31/10/2024: Mandalay added to the shipyard"}], //{expiry: "2024-12-06T00:00:00Z", text: "05/11/2024: Concord Cannon added to the outfitting"}],
+
       language: getLanguage(Persist.getLangCode()),
       route: {},
       sizeRatio: Persist.getSizeRatio()
