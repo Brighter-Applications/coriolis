635ab95ac4e6dab9d1fa0f28e883ea60b3ce04c5
Alex Williams <alex.williams@brighter-technology.com>
2026-03-13 23:07:52 +0000

    Hopefully, fixing linking properly (#242)
    * First commit with cmdr-coriolis links
    
    * fixing linking
    
    * Fixing builds menu throwing an error when the user has no builds
    
    * fixing builds menu (again) and setting up linking ships to builds
    
    * Fixing up the linking modal
    
    * Fixing exports and imports of corsair and T11, as well as some import/export specific stuff not specific to those ships
    
    * Materials needed modal updated
    
    * Modal fixes
    
    * Adding material trader inara link
    
    * fixing build to ship comparison for mats
    
    * fixing missing mats calculation
    
    * debugging linked ships and mat counting
    
    * more debugging
    
    * Hopefully, fixing linking properly

--- src/app/components/ModalShoppingList.jsx
+++ src/app/components/ModalShoppingList.jsx
@@ -148,9 +148,10 @@ export default class ModalShoppingList extends TranslatedComponent {
 
       // Find the linked ship's loadout
       const ships = shipsResp.ships || [];
-      console.log('[Shopping List] Looking for linked ship ID:', linkedBuild.linkedShip);
+      const linkedShipId = linkedBuild.linkedShip?.id || linkedBuild.linkedShip;
+      console.log('[Shopping List] Looking for linked ship ID:', linkedShipId);
       console.log('[Shopping List] Available ships:', ships.map(s => ({ id: s.id, name: s.name, hasLoadout: !!s.loadout })));
-      const linkedShip = ships.find(s => s.id === linkedBuild.linkedShip);
+      const linkedShip = ships.find(s => s.id === linkedShipId);
       console.log('[Shopping List] Found linked ship:', linkedShip ? { id: linkedShip.id, name: linkedShip.name, hasLoadout: !!linkedShip.loadout, loadoutLength: linkedShip.loadout?.length } : 'NOT FOUND');
 
       // Compute remaining mats, comparing with the ship's current loadout
